b597e269abec1deaabf9444bd14bdf325347b407
blakedaniel <blake.vanfleteren@gmail.com>
2023-06-22 09:03:49 -0400

    updated _checkHorWin

--- other/connectK.py
+++ other/connectK.py
@@ -25,22 +25,39 @@ class board:
         
         for col in range(-(win_leng - 1), win_leng):
             possible_columns.append(board[loc + col])
+        
+        left_index = 0
+        final_index = len(possible_columns) - 1
+        right_index = min(win_leng - 1, final_index)
+        
+        while right_index <= final_index:
+            curr_columns = possible_columns[left_index:right_index + 1]
             
-        for row in zip(*possible_columns):
-            player_count = {}
-            curr_player = None
-            for player in row:
+            for row in zip(*curr_columns):
+                player_count = {}
+                curr_player = None
                 
-                if curr_player != player:
-                    curr_player = player
-                    player_count[player] = 0
-                
-                player_count[player] += 1
-                # TODO: update based on Ethan feedback, but what if player placement
-                # causes opponent to win or both to win? Current funciton only checks
-                # for current player winning
-                if player_count[player] == win_leng:
-                    return player
+                for player in row:
+                    
+                    if curr_player != player:
+                        curr_player = player
+                        player_count[player] = 0
+                    
+                    player_count[player] += 1
+                    player1, player2 = player_count.keys()
+                    player1_win = player_count[player1] == win_leng
+                    player2_win = player_count[player2] == win_leng
+                    tie = (player1_win and player2_win)
+                    
+                    if tie:
+                        return player1, player2
+                    elif player1_win:
+                        return player1
+                    elif player2_win:
+                        return player2
+
+            left_index += 1
+            right_index += 1
         return None
         
     def move(self, player:str, loc:int):
